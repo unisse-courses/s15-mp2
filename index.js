96de5353084ae4fbbe7a7c1add9b1e107d9cb27d
BrianJSo <51768646+BrianJSo@users.noreply.github.com>
2020-03-21 22:25:28 +0800

    first commit
    initial try on routing homepage

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+
+const app = express();
+const port = 9090;
+
+app.use(express.static("public"));
+
+app.listen(port, function(){
+    console.log("listening to port " + port);
+});
+
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultView: 'main',
+    layoutsDir: __dirname + "/views/layouts/",
+    partialsDir: __dirname + '/views/partials/'
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/home', function(req, res){
+    res.render('home',{
+        title: "Home",
+        auctions: [
+            {
+                name: "buboi",
+                img: "images/merienda_dp.jpg"
+            },
+            {
+                name: "baboi"
+            }
+        ]
+    })
+});
