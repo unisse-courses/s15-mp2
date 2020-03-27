1db44a9f8a4de168b503b3459844af5159ef7c37
BrianJSo <51768646+BrianJSo@users.noreply.github.com>
2020-03-28 01:22:41 +0800

    created login routes layout and files
    also moved the index data

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const hbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+/* DATA */
+ var auctions = [
+    {
+        sellerName: "meriendacosgrove",
+        location: "Pasay City",
+        sellerImg: "images/merienda_dp.jpg",
+        rating: 4.52,
+        productName: "Chestplate",
+        productImg: "images/chestplate.jpg",
+        categories: ["home", "living", "furniture"],
+        expiryDate: "April 30, 2020",
+        expiryTime: "23:00",
+        bidPrice: 25.00,
+        watchers: 21
+    },
+    {
+        sellerName: "baboi",
+        location: "Pig farm"
+    },
+    {
+        sellerName: "baboi"
+    },
+    {
+        sellerName: "baboi"
+    }
+]
+/* END OF DATA */
+
 app.use(express.static("public"));
 
 app.listen(port, function(){
@@ -19,34 +47,17 @@ app.engine('hbs', hbs({
 
 app.set('view engine', 'hbs');
 
+app.get('/', function(req, res){
+    res.render('login',{ 
+        title: "Login/Register",
+        layout: "login"
+    })
+});
+
 app.get('/home', function(req, res){
     res.render('home',{
         title: "Home",
-        auctions: [
-            {
-                sellerName: "meriendacosgrove",
-                location: "Pasay City",
-                sellerImg: "images/merienda_dp.jpg",
-                rating: 4.52,
-                productName: "Chestplate",
-                productImg: "images/chestplate.jpg",
-                categories: ["home", "living", "furniture"],
-                expiryDate: "April 30, 2020",
-                expiryTime: "23:00",
-                bidPrice: 25.00,
-                watchers: 21
-            },
-            {
-                sellerName: "baboi",
-                location: "Pig farm"
-            },
-            {
-                sellerName: "baboi"
-            },
-            {
-                sellerName: "baboi"
-            }
-        ]
+        auctions
     })
 });
 
