9e75a502de8610e67e9eb1a879c0535b84e7522f
mikobhoie2299 <58876206+mikobhoie2299@users.noreply.github.com>
2020-03-28 04:00:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const hbs = require("express-handlebars");
+const mongodb = require('mongodb');
 
 const app = express();
 const port = 3000;
+
+const mongoClient = mongodb.MongoClient;
+const databaseURL = "mongodb://localhost:27017/";
+const dbname = "laselldb";
 
 app.use(express.static("public"));
 
@@ -49,6 +54,61 @@ app.get('/home', function(req, res){
         ]
     })
 });
+
+
+//Create collections (entities)
+mongoClient.connect(databaseURL, function(err, db){
+
+    if(err) throw err;
+    const dbo = db.db("laselldb");
+
+    dbo.createCollection("students", function(err, res){
+        if (err) throw err;
+        console.log ("Collection created!");
+        db.close();
+    })
+
+})
+
+//CRUD
+
+//Create
+mongoClient.connect(databaseURL, function(err, client){
+
+    if(err) throw err;
+    const dbo = client.db("laselldb");
+
+    const collection = dbo.collection("nameOfEntity");
+
+    //insertOne for single, insertMany for array
+    collection.insertOne(data,/*, [options], */function(err, res){
+        if(err) throw err;
+        console.log("Insert Successful!");
+        client.close();
+    })
+
+})
+
+//Read
+//findOne = retrieves first match
+//find = creates cursor for query to iterate over all results
+//findOneAndDelete
+//findOneAndReplace
+//findOneAndUpdate = one step to delete/replace/update
+//queryObj in the form {field: 'value'}
+mongoClient.connect(databaseURL, function(err,client){
+    if(err) throw err;
+
+    const dbo = client.db("laselldb");
+
+    dbo.collection("nameOfEntity").findOne(queryObj, /*, [options], */ function(err, res){
+    //dbo.collection("nameOfEntity").find({}).toArray(function(err, res){
+        if(err) throw err;
+        console.log(res); //Prints out the document/array of documents
+        client.close();
+    })
+})
+
 
 // app.get('/post/:auctionID', function(req,res){
 //     res.render
