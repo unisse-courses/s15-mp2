8492bc3ae4383b6b578381502393efa9c2298036
mikobhoie2299 <58876206+mikobhoie2299@users.noreply.github.com>
2020-04-05 15:30:57 +0800

    Update watched.js

---
+++ models/watched.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const databaseURL = 'mongodb://localhost:27017/laselldb';
+
+/** README **
+  We need to set useFindAndModify to false because mongoose's findOneAndUpdate
+  is using a deprecated function: findAndModify.
+  This will suppress the warning.
+**/
+const options = { useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false };
+
+mongoose.connect(databaseURL, options);
+
+const watchedSchema = new mongoose.Schema({
+    watcherEmail: { type: String, required: [true, "No email provided"] },
+    auctionID: {type: Number, required: true},
+    bidDate : { type: Date, required: true }
+  }
+  /** README **
+    Virtuals are other fields that do not persist in mongodb.
+    By setting virtuals: true for toObject and toJSON, this makes all the
+    Document.toObject() function include any virtuals value available.
+    For our case, we don't have any.
+  **/
+  // }, {
+  //   toObject: {
+  //     virtuals: true,
+  //   },
+  //   toJSON: {
+  //     virtuals: true,
+  //   }
+  // }
+);
+
+/** README **
+  Export the model as the main content of this module.
+**/
+module.exports = mongoose.model('watched', watchedSchema);
