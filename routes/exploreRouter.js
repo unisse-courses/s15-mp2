7736c5d1ce3cbd53c6e8c881c961125d61ff1a16
BrianJSo <51768646+BrianJSo@users.noreply.github.com>
2020-04-07 01:48:48 +0800

    fixed time display

--- routes/exploreRouter.js
+++ routes/exploreRouter.js
@@ -6,7 +6,16 @@ router.get('/', function(req, res){
     auctionsModel.find({}).populate('sellerID').sort({watchers: 1}).limit(100).exec(function(err, results){
         var auctions = [];
         results.forEach(function(doc){
-            auctions.push(doc.toObject());
+            var curAuction = doc.toObject()
+            var dateObject = curAuction['expiryDate']
+            var hours = ('0' + dateObject.getHours()).slice(-2);
+            var minutes = ('0' + dateObject.getMinutes()).slice(-2);
+
+            curAuction['expiryDate'] =  curAuction.expiryDate.getFullYear()+"-"+
+                                        curAuction.expiryDate.getMonth()+"-"+
+                                        curAuction.expiryDate.getDate()+ " "+
+                                        hours + ":" + minutes;
+            auctions.push(curAuction);
             console.log(auctions);
         })
         res.render('explore',{ title: "Explore", auctions})
