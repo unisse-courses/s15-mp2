8283d0b1ac1dd18feaedf3e2b7eff2968dd12dfc
BrianJSo <51768646+BrianJSo@users.noreply.github.com>
2020-05-07 02:51:16 +0800

    added error handling for invalid user
    added error.hbs for all purpose error goodness

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -12,8 +12,12 @@ exports.selfProfile = function(req, res){
 exports.profile = function(req, res){
     console.log('username = '+ req.params.username);    
     usersModel.getProfileByUsername(req.params.username, function(profile){
-        auctionsModel.getAuctionsBySellerID(profile._id, function(auctions) {
-            res.render('profile',{title: profile.username, profile, auctions})
-        });
+        if(profile){
+            auctionsModel.getAuctionsBySellerID(profile._id, function(auctions) {
+                res.render('profile',{title: profile.username, profile, auctions})
+            });
+        } else {
+            res.render('error',{title: "ERROR", message:"User does not exist:("})
+        }
     });
 };
